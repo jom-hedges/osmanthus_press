b2a715af495b7c6efdedce3a430a62905cc8d536
jom-hedges <jom_hedges@proton.me>
2024-07-09 14:30:39 +0800

    updated the router

--- lib/osmanthus_press_web/router.ex
+++ lib/osmanthus_press_web/router.ex
@@ -18,6 +18,8 @@ defmodule OsmanthusPressWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :home
+
+    live "/posts", PostsLive
   end
 
   # Other scopes may use custom stacks.
